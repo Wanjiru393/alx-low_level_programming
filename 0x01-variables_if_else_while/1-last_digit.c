4e75691063b0d6e078a44412dbb6c73a78e20ff3
Wanjiru393 <carolciru393@gmail.com>
2022-09-09 08:06:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,13 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else
+	else if (l < 6 && l != 0)
 	{
 		printf("Last digit 0f %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	else
+	{
+		printf(" ");
 	}
 	return (0);
 }
