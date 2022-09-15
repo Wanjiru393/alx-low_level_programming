745e6793ec996a5d7d82cc8c061ef8e03c4b7d22
Wanjiru393 <carolciru393@gmail.com>
2022-09-15 15:24:26 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-int num;
+	int i;
 
-for (num = 0; num <= 9; num++)
-_putchar((num % 10) + '0');
+	for (i = 0; i <= 9; i++)
+		_putchar((i % 10) + '0');
 
-_putchar('\n');
+	_putchar('\n');
 }
