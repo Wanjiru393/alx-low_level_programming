00983fe7daccccdeb0c98107764aa91d25e6a54a
Wanjiru393 <carolciru393@gmail.com>
2022-09-19 10:43:32 -0700

    Reset file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File:main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
